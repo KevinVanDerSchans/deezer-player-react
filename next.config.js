e700fe226e1d7c096d6be82ffd38ac95e9636df0
Kevin Schans <kevinvdsd@hotmail.com>
2024-01-19 23:09:10 +0100

    Add trending tracks funcionality with responsive styles

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ['e-cdns-images.dzcdn.net'],
+  },
   experimental: {
     appDir: true,
   },
---
+++ src/app/components/TrendingTracks/TrendingTracks.jsx
@@ -0,0 +1,29 @@
+import TrackItem from '../TrackItem/TrackItem';
+
+const TrendingTracks = ({ header, tracks }) => {
+  return (
+    <section className="trending-tracks">
+
+      <div className='trending-tracks-header'>
+        { !! header && <h2>{ header }</h2> }
+      </div>
+
+      <div className='trending-tracks-container'>
+        <ul className='trending-tracks-list'>
+          {
+            tracks.map(( track, index) =>
+              <TrackItem
+                key={ track.id }
+                index= { index }
+                playlist={ tracks }
+                track={ track }
+              />
+            )
+          }
+        </ul>
+      </div>
+    </section>
+  )
+}
+
+export default TrendingTracks;

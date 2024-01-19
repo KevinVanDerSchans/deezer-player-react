8fd941b927d66d9a0a1dbe2f66f73696d1763946
Kevin Schans <kevinvdsd@hotmail.com>
2024-01-19 18:12:36 +0100

    Fix error

--- src/app/components/Header/Header.jsx
+++ src/app/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 'use client';
 import Link from "next/link";
 import Image from "next/image";
-import Navbar from '../Navbar/Navbar'
+import Navbar from '../Navbar/Navbar.jsx'
 
 const Header = () => {
   return (
--- src/app/components/navbar/navbar.jsx
+++ src/app/components/navbar/navbar.jsx
@@ -1,4 +1,4 @@
-import NavbarLink from './NavbarLink';
+import NavbarLink from './NavbarLink'
 
 const Navbar = () => {
   return (

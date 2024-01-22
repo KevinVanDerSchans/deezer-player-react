0abb7a4c200c0392031d0c1a0ddcd9b647334a4a
Kevin Schans <kevinvdsd@hotmail.com>
2024-01-22 16:05:52 +0100

    No

--- src/app/components/Favorites/ArtistsTabContent.jsx
+++ src/app/components/Favorites/ArtistsTabContent.jsx
@@ -1,26 +1,36 @@
 import useSWR from 'swr';
 import LinkCardItem from './LinkCardItem';
 
-const ArtistsTabContent = ({ artists }) => {
+const ArtistsTabContent = ({ header, artists }) => {
   const { data } = useSWR({ entitiesIds: artists, endpoint: '/api/artist' });
 
   return (
-    <ul>
-      {
-        data.map((artist => {
-          const { id, name, picture_medium } = artist;
+    <section className="trending-tracks">
 
-          return (
-            <LinkCardItem
-              key={ id }
-              title={ name }
-              imgSrc= { picture_medium }
-              href={ `/artist/${ id }` }
-            />
-          );
-        }))
-      }
-    </ul>
+      <div className='trending-tracks-header'>
+        { !! header && <h1>{ header }</h1> }
+      </div>
+
+      <div className='trending-tracks-container'>
+
+        <ul className='trending-tracks-list'>
+          {
+            data.map((artist => {
+              const { id, name, picture_medium } = artist;
+
+              return (
+                <LinkCardItem
+                  key={ id }
+                  title={ name }
+                  imgSrc= { picture_medium }
+                  href={ `/artist/${ id }` }
+                />
+              );
+            }))
+          }
+        </ul>
+      </div>
+    </section>
   );
 };
 
--- src/app/components/Favorites/LinkCardItem.jsx
+++ src/app/components/Favorites/LinkCardItem.jsx
@@ -2,18 +2,20 @@
 import Link from 'next/link';
 import React from 'react';
 
-const LinkCardItem = ({ href, imgSrc, title, description }) => {
+const LinkCardItem = ({ href, imgSrc, title }) => {
   return (
-    <li className='card-container'>
-      <Link href={ href }>
-        <img src={ imgSrc } alt="" />
-        <strong>{ title }</strong>
+    <li className='track-list-item'>
+      <div className='track-list-item-container artist'>
 
-        {
-          description &&
-            <small>{ description }</small>
-        }
-      </Link>
+        <div className='trending-track-details'>
+          <div className='trending-track-details-text'>
+              <Link href={ href }>
+                <img src={ imgSrc } alt="Artist image" className='trending-item-image' />
+                <span className='favorite-artist-title'>{ title }</span>
+              </Link>
+            </div>
+          </div>
+      </div>
     </li>
   );
 };

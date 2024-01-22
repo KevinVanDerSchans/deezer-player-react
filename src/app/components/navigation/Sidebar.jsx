97ccfc376502a7d2bf608c678d23054675f5230c
Kevin Schans <kevinvdsd@hotmail.com>
2024-01-22 22:48:12 +0100

    Adjust tracks style

--- src/app/components/navigation/Sidebar.jsx
+++ src/app/components/navigation/Sidebar.jsx
@@ -9,7 +9,14 @@ const Sidebar = () => {
 
       <div className="logoAndSearch">
         <Link href="/" className="logo">
-          <Image src="/assets/svg/deezer-logo.svg" alt="Deezer logo" width={148} height={28} priority />
+          <Image
+            className='logo'
+            src="/assets/svg/deezer-logo.svg"
+            alt="Deezer logo"
+            width={148}
+            height={28}
+            priority
+          />
         </Link>
 
         <Search />

f8ab3d0477d5ffb03a014180251be94dbbcf2e68
Kevin Schans <kevinvdsd@hotmail.com>
2024-01-23 04:25:07 +0100

    Add reveal style to pages

--- src/app/components/Favorites/TabContentContainer.jsx
+++ src/app/components/Favorites/TabContentContainer.jsx
@@ -1,3 +1,5 @@
+import Reveal from '../../utils/reveal';
+
 const TabContentContainer = ({ entities, children }) => {
   const isEmpty = ! entities.length;
 
@@ -5,14 +7,16 @@ const TabContentContainer = ({ entities, children }) => {
     <>
       {
         isEmpty ?
-          <section className="empty-list-section">
-            <div className="empty-list-container">
-              <h2 className="empty-list-title">Empty favorites !</h2>
-              <h3 className="empty-list-description">
-                Browse the <strong>APP</strong> and search for your favorite <strong>songs</strong> !
-              </h3>
-            </div>
-          </section>
+          <Reveal duration={2} delay={0.2} >
+            <section className="empty-list-section">
+              <div className="empty-list-container">
+                <h2 className="empty-list-title">Empty favorites !</h2>
+                <h3 className="empty-list-description">
+                  Browse the <strong>APP</strong> and search for your favorite <strong>songs</strong> !
+                </h3>
+              </div>
+            </section>
+          </Reveal>
         :
         <>
           { children }
---
+++ src/app/utils/reveal.js
@@ -0,0 +1,42 @@
+import { motion, useAnimation, useInView } from 'framer-motion';
+import { useEffect, useRef } from 'react';
+
+export default function Reveal(props) {
+  const {
+    children,
+    opacity = 0,
+    y = 0,
+    x = 0,
+    blur = 0,
+    duration = 0.5,
+    delay = 0.5,
+    rotate = 0,
+  } = props;
+
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  const mainControls = useAnimation();
+
+  useEffect(() => {
+    if (isInView) {
+      mainControls.start('visible');
+    }
+  }, [isInView, mainControls]);
+
+  return (
+    <motion.div
+      ref={ref}
+      style={{ position: 'relative', overflow: 'hidden' }}
+      variants={{
+        hidden: { filter: `blur(${blur}px)`, opacity, y, x, rotate },
+        visible: { filter: 'blur(0)', opacity: 1, y: 0, x: 0, rotate: 0 },
+      }}
+      initial="hidden"
+      animate={mainControls}
+      transition={{ duration, delay }}
+    >
+      {children}
+    </motion.div>
+  );
+}
